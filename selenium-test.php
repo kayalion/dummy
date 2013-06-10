b86dce3f3fc5101adb3a4c4b752cbe3529279401
Joris Vandeweerd <joris@statik.be>
2013-06-11 01:34:09 +0200

    added selenium test

---
+++ selenium-test.php
@@ -0,0 +1,13 @@
+<?php
+class WebTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+    protected function setUp() {
+        $this->setBrowser('firefox');
+    }
+ 
+    public function testTitle() {
+        $this->url('http://www.google.com/');
+        $this->assertEquals('Google', $this->title());
+    }
+ 
+}
